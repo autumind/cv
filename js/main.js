20cee86db689e8bbc74040978154df317125e3e4
zb.shen <autumind@gmail.com>
2016-07-20 10:35:02 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,50 @@
-var ButtonToolbar = AMUIReact.ButtonToolbar;
-var Button = AMUIReact.Button;
-var buttonsInstance = (
-  <ButtonToolbar>
-    <Button amStyle="primary">Primary</Button>
-    <Button amStyle="secondary">Secondary</Button>
-    <Button amStyle="success">Success</Button>
-    <Button amStyle="warning">Warning</Button>
-    <Button amStyle="danger">Danger</Button>
-  </ButtonToolbar>
-);
- 
- ReactDOM.render(
-//  <h1>Hello, world!</h1>,
-	buttonsInstance,
-    document.getElementById('cv')
+var Me = React.createClass({
+	render: function () {
+		return (
+			<div className="me">
+				<h1 className="am-article-title me-name">{this.props.name}</h1>
+				<p className="am-article-meta me-title">{this.props.title}</p>
+				<p className="me-desc">{this.props.desc}</p>
+			</div>
+		);
+	}
+});
+
+var ItemTitle = React.createClass({
+	render: function () {
+		return (
+			<i className={this.props.className}>  {this.props.name}</i>
+		);
+	}
+});
+
+var Photo = React.createClass({
+	render: function () {
+		return (
+			<div className="photoOut">
+				<img src={this.props.src} alt="aaa" className="photoIn am-center"/>
+			</div>
+		);
+	}
+});
+
+var Cv = React.createClass({
+	render: function () {
+		return (
+			<div className="main">
+			  <div className="am-u-sm-7 left">
+				<Me name={profile.name} title={profile.title} desc={profile.desc}/>
+				<ItemTitle className="am-icon-institution" name="JOB EXPERIENCE"/>
+			  </div>
+			  <div className="am-u-sm-5 right">
+			  	<Photo src="http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/1000/h/1000/q/80"/>
+			  </div>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<Cv /> ,
+	document.getElementById('cv')
 );
