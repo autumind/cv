2089a61df224b03908a137db06d0d85f9d6f051e
zb.shen <autumind@gmail.com>
2016-07-20 10:35:02 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
-var ButtonToolbar = AMUIReact.ButtonToolbar;
-var Button = AMUIReact.Button;
-var buttonsInstance = (
-  <ButtonToolbar>
-    <Button amStyle="primary">Primary</Button>
-    <Button amStyle="secondary">Secondary</Button>
-    <Button amStyle="success">Success</Button>
-    <Button amStyle="warning">Warning</Button>
-    <Button amStyle="danger">Danger</Button>
-  </ButtonToolbar>
-);
- 
- ReactDOM.render(
-//  <h1>Hello, world!</h1>,
-	buttonsInstance,
-    document.getElementById('cv')
+var ItemTitle = React.createClass({
+	render: function () {
+		return (
+			<i className={this.props.className}>  {this.props.name}</i>
+		);
+	}
+});
+
+var Photo = React.createClass({
+	render: function () {
+		return (
+			<img src={this.props.src} alt="aaa" className="am-img-thumbnail am-radius am-center"/>
+		);
+	}
+});
+
+var Cv = React.createClass({
+	render: function () {
+		return (
+			<div className="main">
+			  <div className="am-u-sm-7 left">
+			  	<ItemTitle className="am-icon-home am-icon-sm" name="技能"/>
+			  </div>
+			  <div className="am-u-sm-5 right">
+			  	<Photo src="http://s.amazeui.org/media/i/demos/bw-2014-06-19.jpg?imageView/1/w/1000/h/1000/q/80"/>
+			  </div>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<Cv /> ,
+	document.getElementById('cv')
 );
