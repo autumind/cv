dab05d74b6df53b904223ef9aefa56bfa6423a0e
reno.shen <autumind@gmail.com>
2016-08-04 19:05:58 +0800

    fix company desc

--- js/ch.js
+++ js/ch.js
@@ -14,21 +14,25 @@ var ch = {
 						start    : "2015年12月",
 						end      : "至今",
 						position : "高级Java开发工程师",
-						content  : "xxxxxx"
+						content  : ["在司半年至今，参与开发了微信企业号微移云产品、年轻妈妈APP。",
+									"微信企业号主要面向企业用户，涵盖了企业管理中的考勤，请假，通知，报销，审批等常用流程，并提供定制服务，开发涉及SpringMVC、MySQL、H5、CSS3、Javascript、缓存、nginx等技术；",
+									"年轻妈妈APP主要面向怀孕及刚生育妈妈的在线问诊并提供孕食推荐，月子会所订购等服务，开发涉及除微移云相关以外，还涉及H5 Hybird、DCloud壳应用、H5PLUS等技术。"]
 					},
 					{
 						company  : "富士通南大软件技术有限公司",
 						start    : "2012年12月",
 						end      : "2015年11月",
 						position : "普通开发",
-						content  : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
+						content  : ["在司三年，前一年半时间主要参与对日项目，其中包括物流系统，订单系统，涉及Action Script 3、Java、SQL等相关技术，并且参与了一些UI，SS设计。",
+									"其中出差日本期间，掌握了一种有富士通自有主导的开发语言PRO IV，并参与开发了Glovia相关功能，之后作为技术顾问对后参与同事进行了培训。",
+									"后一半时间主要参与国内项目，包括某市旅游推介及点评系统，中小型制造业出入库管理系统，政府单位行政处罚系统，某幼儿园资源管理系统，域名拍卖系统，涉及SpringMVC、H5、CSS3、SQL、Maven、微信平台等相关技术。"]
 					},
 					{
 						company  : "瞬联科技",
 						start    : "2012年07月",
 						end      : "2012年11月",
 						position : "Android系统开发工程师",
-						content  : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
+						content  : ["在司4个月期间，参加Android Framework相关培训，并且参与了SMS模块双卡双待的代码改造。"]
 					}
 				],
 	edu      : [
@@ -39,7 +43,9 @@ var ch = {
 						major  : "计算机科学与技术",
 						degree : "学士",
 						where  : "江苏，南京",
-						desc   : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
+						desc   : ["整个大学期间，学习为主，基础课程基本高分通过，毕业绩点班级前三。",
+								  "作为新闻记者参加过社团，获得过国家、校级等各类奖助学金。",
+								  "为学校2012年优秀毕业生。"]
 					}
 				],
 	skill    : [
