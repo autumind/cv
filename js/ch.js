ed3fdef644051f22e7e1158c13453656bf1ef94f
reno.shen <autumind@gmail.com>
2016-08-04 18:17:47 +0800

    fix me desc

--- js/ch.js
+++ js/ch.js
@@ -1,16 +1,13 @@
 var ch = {
 	name     : "沈志兵",
 	title    : "高级开发工程师",
-	desc     : ["Success is not final and failure is not fatal:" , 
-		        " it is the courage to continue that counts. " , 
-		        "Never cease to outdo yourself. " , 
-		        "There are no limits to the success you can acquire" , 
-		        " when you have an duanwenw.com uncontrollable drive to achieve." , 
-		        " Any obstacles you encounter along the way are life’s test" , 
-		        " to establish strength and perseverance. " , 
-		        "If you continually possess determination and " , 
-		        "you"],
-	quote    : "要是用几行字就能概括我，我会没脸活下去的",
+	desc     : ["和绝大多数志愿长大成为科学家的小朋友一样，毫无意外地没有成为科学家，我成为了程序员。" , 
+		        "在圈内这听起来好像有点绝望，但是从农村包围城市的战略上讲，这也是一种胜利。" , 
+		        "毕业至今四年多一点，与代码遭遇的点滴虽然没有历历在目，却也没有成为过眼烟云。" , 
+		        "不是技术大牛，总也朝着全栈工程师的方向努力；不是技术狂热分子，却对解决问题有着无比的执着；不是代码终结者，但也想让代码更美丽。" , 
+		        "热爱生活，希望通过努力可以让自己去超市不看价格；喜欢冒险，一直想去蹦极或者跳伞；崇尚简单，我所拥有的很少超过两种颜色。" , 
+		        "偶尔张狂，偶尔深沉，不一定能飞黄腾达，但也会不遗余力的把这一生过得更丰富多彩。"],
+	quote    : "其实要是用几行字就能概括我，我会没脸活下去的 :-)",
 	job      : [
 					{
 						company  : "江苏优客互联科技股份有限公司",
@@ -24,14 +21,14 @@ var ch = {
 						start    : "2012年12月",
 						end      : "2015年11月",
 						position : "普通开发",
-						content  : "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+						content  : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
 					},
 					{
 						company  : "瞬联科技",
 						start    : "2012年07月",
 						end      : "2012年11月",
 						position : "Android系统开发工程师",
-						content  : "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+						content  : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
 					}
 				],
 	edu      : [
@@ -42,7 +39,7 @@ var ch = {
 						major  : "计算机科学与技术",
 						degree : "学士",
 						where  : "江苏，南京",
-						desc   : "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+						desc   : ["XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"]
 					}
 				],
 	skill    : [
